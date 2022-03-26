23d2b714bb66b3cc02efa12f1ecd0f6780b0279e
asya <asyatim@mail.ru>
2022-03-26 14:44:27 +0600

    initialized values

--- src/main/java/ru/geekbrains/lesson1/MyApp.java
+++ src/main/java/ru/geekbrains/lesson1/MyApp.java
@@ -2,6 +2,14 @@ package ru.geekbrains.lesson1;
 
 public class MyApp {
     public static void main(String[] args) {
+        byte byteVal = 127;
+        int intVal = 127;
+        short shortVal = 127;
+        long longVal = 127000L;
+        float floatVal = 127.01f;
+        double doubleVal = 127.012;
+        char charVal = '*';
+        boolean booleanVal = true;
 
     }
 }

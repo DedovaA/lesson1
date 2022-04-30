0cfb1eaf5e38a27da9a9d605a3eaca0f2935b230
asya <asyatim@mail.ru>
2022-05-01 04:22:13 +0600

    lesson12, tried to implement  ExecutorService in method3.

--- src/main/java/ru/geekbrains/lesson12/lesson12.java
+++ src/main/java/ru/geekbrains/lesson12/lesson12.java
@@ -1,12 +1,15 @@
 package ru.geekbrains.lesson12;
 
+import java.util.concurrent.*;
+
 public class lesson12 {
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         method1();
         method2();
+        method3();//the same method2 with executorService
 
     }
 
@@ -32,8 +35,6 @@ public class lesson12 {
         System.out.println(System.currentTimeMillis()-start);
     }
 
-
-
     static void method2 () throws InterruptedException {
         float[] arrFull = createFloatArray(SIZE);
 
@@ -41,6 +42,7 @@ public class lesson12 {
 
         float[] arr1 = new float[HALF];
         float[] arr2 = new float[HALF];
+
         System.arraycopy(arrFull, 0, arr1, 0, HALF);
         System.arraycopy(arrFull, HALF, arr2, 0, HALF);
 
@@ -54,9 +56,29 @@ public class lesson12 {
 
         System.arraycopy(arr1, 0, arrFull, 0, HALF);
         System.arraycopy(arr2, 0, arrFull, HALF, HALF);
-
-        System.out.println(System.currentTimeMillis()-start);
+        System.out.println(System.currentTimeMillis() - start);
     }
 
+    static void method3 () {
+        float[] arrFull = createFloatArray(SIZE);
+
+        long start = System.currentTimeMillis();
+        float[] arr1 = new float[HALF];
+        float[] arr2 = new float[HALF];
+        System.arraycopy(arrFull, 0, arr1, 0, HALF);
+        System.arraycopy(arrFull, HALF, arr2, 0, HALF);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.execute(() -> {
+            calculate(arr1);
+            calculate(arr2);
+        });
+        executorService.shutdown();
+
+        System.arraycopy(arr1, 0, arrFull, 0, HALF);
+        System.arraycopy(arr2, 0, arrFull, HALF, HALF);
+        System.out.println(System.currentTimeMillis()-start);
+
+    }
 
 }
